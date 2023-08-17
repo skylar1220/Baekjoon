08c73a2eeda23bcd4e8d5908b8458d0a4dfd6b51
Hyeonji <110809927+skylar1220@users.noreply.github.com>
2023-08-18 08:37:51 +0900

    [Silver IV] Title: 설탕 배달, Time: 128 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,33 +1,35 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
-public class Main {
-    public static void main(String[] args) {
-    	
-    	Scanner sc = new Scanner(System.in);
-    	int n = sc.nextInt() ;
-    	int cnt = 0 ;
-    	int cntTmp = 0 ;
-    	
-    	if(n % 5 ==  0) {
-    		cnt = cnt + n / 5 ; 
-    		System.out.println(cnt);
-    	} else {
-	    	int tmp = n / 3 ;
-	    	for (int i = 0; i < tmp  ; i++) {
-				n = n-3 ;
-				cntTmp ++ ;
-				if( n % 5 == 0 ) break ; 
-			}
-	    	if(n % 5 != 0 ) {
-	    		System.out.println(-1);
-	    	}
-	    	else {
-	    	cnt = cntTmp ; 
-	    	cnt = cnt + n / 5 ; 
-	    	
-	    	
-	    	System.out.println(cnt);
-    	}
-    	}
+public class Main{
+    static int N;
+    static int res = -1;
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+
+        // 5킬로그램을 가장 많이 가져가는 것이 최대한 적은 봉투 수
+        // 따라서 가장 많이 5킬로그램을 가져갈 수 있는 경우의 수에서 점점 줄여나가기
+        int fiveNum = N/5;
+        int threeNum = 0;
+
+        // 5킬로그램 봉투가 0보다 작을 경우 만족하는 조건이 없으므로 -1 출력
+        while(fiveNum>=0){
+            int remain = N - (fiveNum*5);
+
+            // 5킬로그램 다 들고 남은 개수가 3킬로그램 단위로 모두 나눌 수 있는 경우 정답 출력
+            if(remain%3==0){
+                threeNum = remain/3;
+                res = fiveNum + threeNum;
+                break;
+            }
+
+            // 5킬로그램 봉투 개수 줄이기
+            fiveNum--;
+        }
+
+        System.out.println(res);
     }
 }
